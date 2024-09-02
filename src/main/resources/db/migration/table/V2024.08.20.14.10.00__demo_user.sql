f0406601c0fde6f7e27490cea94cfc1cb1154e57
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-09-02 14:06:22 +0200

    Migration

---
+++ src/main/resources/db/migration/table/V2024.08.20.14.10.00__demo_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, name, email, password, created_at) VALUES ('def32435-e15c-4561-b5dc-3e70efbff1c0', 'Demo User', 'demo@demo.demo', '$2a$10$g80utLk5ah3kVwbOnVhlT.889slVnhIwyY60.F7mn8BTYS.bgYeGG', NOW());
+
+INSERT INTO users_roles (user_id, role_id) VALUES ('def32435-e15c-4561-b5dc-3e70efbff1c0', '37332435-e15c-4561-b5dc-3e70efbff1c0');
